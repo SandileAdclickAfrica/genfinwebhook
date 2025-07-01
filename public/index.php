316d446c0b3813c659ddc086f0659944a23ba012
Sandile Developer <sandile@adclickafrica.com>
2025-07-01 10:03:14 +0200

    adding dotenv file

--- public/index.php
+++ public/index.php
@@ -3,12 +3,26 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
+use Dotenv\Dotenv;
+
 require __DIR__ . '/../vendor/autoload.php';
+
+// Load environment variables
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+
+require __DIR__ . '/../vendor/autoload.php';
+
+
+
+
 
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world! Test");
+    $env = $_ENV['S3_BUCKET']; // 'local'
+    $response->getBody()->write( $env );
     return $response;
 });
 
@@ -16,5 +30,9 @@ $app->get('/about', function ($request, $response, $args) {
     $response->getBody()->write("This is the About page.");
     return $response;
 });
+
+
+
+
 
 $app->run();
